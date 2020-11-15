12ebca21f37fb866a16abb5d9a8ade7db32ea6d8
KimCottrell <kimc2263@gmail.com>
2020-11-15 06:43:47 -0500

    ChallengeSet01 complete

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -5,32 +5,80 @@ namespace ChallengeSets
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+
+            }
+
+            //throw new NotImplementedException();
         }
 
         public double Subtract(double minuend, double subtrahend)
+
+
         {
-            throw new NotImplementedException();
+            //var sub1 = minuend - subtrahend;
+            //return sub1;
+            return minuend - subtrahend;
+            //throw new NotImplementedException();
         }
 
         public int Add(int number1, int number2)
+
+
         {
-            throw new NotImplementedException();
+            //var add1 = number1 + number2;
+            //return add1;
+            return number1 + number2;
+            //throw new NotImplementedException();
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+
+            }
+            else 
+
+            return number2;
+
+
+            //throw new NotImplementedException();
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+
+            //throw new NotImplementedException();
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+
+            if (nameOfPerson.Length > 0)
+
+            {
+                return "Hello, " + nameOfPerson + "!";
+
+            }
+            else
+            {
+                return "Hello!";
+
+            }
+
+            
+
+            //throw new NotImplementedException();
         }
 
         public string GetHey()
